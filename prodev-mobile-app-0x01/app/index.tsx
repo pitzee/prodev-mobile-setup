cd80db9d938af6ff0b8dc1baf403a87f46d2b559
pitzee <pitzee421@gmail.com>
2025-08-26 20:25:59 +0300

    updated

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -5,16 +5,12 @@ export default function Index() {
     <View style={styles.container}>
       <Text>"Entry Screen - Awesome"</Text>
       <Text style={styles.largeText}>
-        Typescript is great if you practice more{" "}
+        Typescript is great if you practice more
       </Text>
       <Text style={styles.mediumText}>
-        React Native provides you a single codebase for cross platforms{" "}
+        React Native provides you a single codebase for cross platforms
       </Text>
       <Text style={styles.smallText}>ALX is awesome</Text>
-
-      <Text style={styles.largeText}>Hello ALX</Text>
-      <Text style={styles.mediumText}>React Native is fun</Text>
-      <Text style={styles.smallText}>Practice makes perfect</Text>
     </View>
   );
 }
